a4e083b2552f7f62dcc0fd8f4366e0fcf4f3c402
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2023-04-04 20:39:17 -0700

    reduce margin size of bios to 0

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function togglebio(elementId) {
-  
   let bios = document.querySelectorAll('.bio');
   bios.forEach(bio => {
     bio.classList.remove('bio--open');
   });
 
- 
-  let element = document.getElementById(elementId)
-  
-  element.classList.add("bio" + "--open");
+  let element = document.getElementById(elementId);
+
+  if (!element.classList.contains('bio--open')) {
+    element.classList.add('bio--open');
+  } else {
+    element.classList.remove('bio--open');
+  }
 }
 
 function exitbio(elementId) {
